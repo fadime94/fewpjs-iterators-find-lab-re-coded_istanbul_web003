ed644cb89fba27cc106b0d7baf75ec912ee0604e
fadime94 <fadime.ozdemir11@gmail.com>
2020-08-09 16:39:16 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records){
+   const record = records.find(record=>{
+     return record.result==="W";
+    });
+    if(record){
+      return record.year;
+    }else {
+      return undefined;
+    }
+    
+}
+superbowlWin(records)
